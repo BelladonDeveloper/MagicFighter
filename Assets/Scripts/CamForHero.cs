1b3eb57235dfb03b1b513c51f9cfb27f3025b7b9
BelladonDeveloper <47111877+BelladonDeveloper@users.noreply.github.com>
2019-11-30 14:47:24 +0200

    Add files via upload

---
+++ Assets/Scripts/CamForHero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CamForHero : MonoBehaviour
+{
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(new Vector3(target.position.x, target.position.y + 0.75f, target.position.z));
+            Vector3 delta = new Vector3(target.position.x, target.position.y + 0.75f, target.position.z) - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float SPEED = 400f;
+    public float HEIGHT = 18200f;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    private new Rigidbody2D rigidbody;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float isMove;
+    private float groundRadius = 0.8f;
+    public bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        
+        isMove = Input.GetAxis("Horizontal");
+        go();
+
+        if (Input.GetButtonDown("Fire1")) Shoot();
+        if (Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Jump()
+    {
+        if (grounded) rigidbody.AddForce(transform.up * HEIGHT, ForceMode2D.Impulse);
+    }
+
+    private void Shoot()
+    {
+        anim.SetTrigger("Shoot");
+    }
+
+    private void go()
+    {
+        rigidbody.velocity = new Vector2(isMove * SPEED, rigidbody.velocity.y);
+
+        if (isMove == 0)
+        {
+            anim.SetBool("IsRun", false);
+        }
+        else
+        {
+            anim.SetBool("IsRun", true);
+        }
+        if (isMove < 0)
+        {
+            sprite.flipX = true;
+        }
+        if (isMove > 0)
+        {
+            sprite.flipX = false;
+        }        
+    }
+}
